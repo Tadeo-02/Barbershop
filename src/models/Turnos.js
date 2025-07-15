0cd33580c711bd47b08379084daccf32669d77d5
Tadeo-02 <153774639+Tadeo-02@users.noreply.github.com>
2025-07-15 16:50:57 -0300

    CRUD Nº1
    - Re-estructuración de archivos para integrar backend
    - Añadido de archivos necesarios para CRUD Turnos
    - Instalación de modulos: express, dotEnv, methodOverride

---
+++ src/models/Turnos.js
@@ -0,0 +1,65 @@
+const pool = require("./mysql");
+
+const store = async (fechaTurno) => {
+  const sql = `INSERT INTO turnos (fechaTurno) VALUES (?)`;
+
+  try {
+    const [result] = await pool.query(sql, [fechaTurno]);
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const findAll = async () => {
+  const sql = "SELECT * FROM turnos";
+
+  try {
+    const [rows] = await pool.query(sql);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const findById = async (codTurno) => {
+  const sql = `SELECT * FROM turnos WHERE codTurno = ?`;
+
+  try {
+    const [rows] = await pool.query(sql, [codTurno]);
+    // console.log(rows, rows.shift())
+    return rows.shift();
+  } catch (error) {
+    throw error;
+  }
+};
+
+const update = async (codTurno, fechaTurno) => {
+  const sql = `UPDATE turnos SET fechaTurno = ? WHERE codTurno = ?`;
+
+  try {
+    const [result] = await pool.query(sql, [fechaTurno, codTurno]);
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const destroy = async (codTurno) => {
+  const sql = `DELETE FROM turnos WHERE codTurno = ?`;
+
+  try {
+    const [result] = await pool.query(sql, [codTurno]);
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  store,
+  findAll,
+  findById,
+  update,
+  destroy,
+};
--- src/views/components/turnos/mainTurnos.tsx
+++ src/views/components/turnos/mainTurnos.tsx
@@ -27,21 +27,42 @@ function MainTurnos() {
         </Routes>
       </div>
       */
-     /*MUESTRA DE SUCURSALES */
+    /*MUESTRA DE SUCURSALES */
     <div>
       <div className={styles.container}>
+        <Link to="/createTurnos" className={styles.link}>
+          CREAR TURNOS
+        </Link>
         <h2 className={styles.title}>Sucursal Centro</h2>
         <div className={styles.branchGallery}>
-          <img src="/images/sucursal1.jpeg" alt="Sucursal Centro" className={styles.branchImage} />
+          <img
+            src="/images/sucursal1.jpeg"
+            alt="Sucursal Centro"
+            className={styles.branchImage}
+          />
         </div>
-        <a href="https://maps.app.goo.gl/ut38V2Tf414qxqs28" className={styles.mapLink}>Dirección: Mendoza 2774, Rosario </a>
+        <a
+          href="https://maps.app.goo.gl/ut38V2Tf414qxqs28"
+          className={styles.mapLink}
+        >
+          Dirección: Mendoza 2774, Rosario{" "}
+        </a>
       </div>
       <div className={styles.container}>
         <h2 className={styles.title}>Sucursal Norte</h2>
         <div className={styles.branchGallery}>
-          <img src="/images/sucursal2.jpg" alt="Sucursal Norte" className={styles.branchImage} />
+          <img
+            src="/images/sucursal2.jpg"
+            alt="Sucursal Norte"
+            className={styles.branchImage}
+          />
         </div>
-        <a href="https://maps.app.goo.gl/KJnih2u2hf5S9jmB6" className={styles.mapLink}>Dirección: Juan B. Justo 1767, Rosario </a>
+        <a
+          href="https://maps.app.goo.gl/KJnih2u2hf5S9jmB6"
+          className={styles.mapLink}
+        >
+          Dirección: Juan B. Justo 1767, Rosario{" "}
+        </a>
       </div>
     </div>
   );
