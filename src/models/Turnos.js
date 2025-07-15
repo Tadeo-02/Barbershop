b86b174efae176acb72ddeeb8cf9ce97c4223e59
matimorici <morici.mati@gmail.com>
2025-07-15 19:46:19 -0300

    Comentarios

--- src/models/Turnos.js
+++ src/models/Turnos.js
@@ -11,55 +11,55 @@ const store = async (fechaTurno) => {
   }
 };
 
-const findAll = async () => {
-  const sql = "SELECT * FROM turnos";
+// const findAll = async () => {
+//   const sql = "SELECT * FROM turnos";
 
-  try {
-    const [rows] = await pool.query(sql);
-    return rows;
-  } catch (error) {
-    throw error;
-  }
-};
+//   try {
+//     const [rows] = await pool.query(sql);
+//     return rows;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
-const findById = async (codTurno) => {
-  const sql = `SELECT * FROM turnos WHERE codTurno = ?`;
+// const findById = async (codTurno) => {
+//   const sql = `SELECT * FROM turnos WHERE codTurno = ?`;
 
-  try {
-    const [rows] = await pool.query(sql, [codTurno]);
-    // console.log(rows, rows.shift())
-    return rows.shift();
-  } catch (error) {
-    throw error;
-  }
-};
+//   try {
+//     const [rows] = await pool.query(sql, [codTurno]);
+//     // console.log(rows, rows.shift())
+//     return rows.shift();
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
-const update = async (codTurno, fechaTurno) => {
-  const sql = `UPDATE turnos SET fechaTurno = ? WHERE codTurno = ?`;
+// const update = async (codTurno, fechaTurno) => {
+//   const sql = `UPDATE turnos SET fechaTurno = ? WHERE codTurno = ?`;
 
-  try {
-    const [result] = await pool.query(sql, [fechaTurno, codTurno]);
-    return result;
-  } catch (error) {
-    throw error;
-  }
-};
+//   try {
+//     const [result] = await pool.query(sql, [fechaTurno, codTurno]);
+//     return result;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
-const destroy = async (codTurno) => {
-  const sql = `DELETE FROM turnos WHERE codTurno = ?`;
+// const destroy = async (codTurno) => {
+//   const sql = `DELETE FROM turnos WHERE codTurno = ?`;
 
-  try {
-    const [result] = await pool.query(sql, [codTurno]);
-    return result;
-  } catch (error) {
-    throw error;
-  }
-};
+//   try {
+//     const [result] = await pool.query(sql, [codTurno]);
+//     return result;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
 module.exports = {
   store,
-  findAll,
-  findById,
-  update,
-  destroy,
+  // findAll,
+  // findById,
+  // update,
+  // destroy,
 };
