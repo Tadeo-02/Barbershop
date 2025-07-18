8ae58bddd3492c29755bd50c483eb9177b969d24
dolodoming <doloodominguez@gmail.com>
2025-07-18 16:29:41 -0300

    CRUD Categorias
    - Creación del controlador, las rutas, los modelos de la BD.
    - Creacion de los .tsx create, delete, modificar e index.
    - Desarrollo del Create

---
+++ src/routes/categorias.router.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const controller = require("../controllers/categorias.controller");
+
+router.get("/createCategorias", controller.create);
+router.post("/", controller.store);
+
+router.get("/", controller.index);
+router.get("/:codCategoria", controller.show);
+
+router.get("/:codCategoria/modificarCategorias", controller.edit);
+router.put("/:codCategoria", controller.updateDesc, controller.updateNom);
+
+router.delete("/:codCategoria", controller.destroy);
+// 
+module.exports = router;
