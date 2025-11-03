17d4487fc60b7155bac697242120609123f31067
dolodoming <doloodominguez@gmail.com>
2025-11-03 17:06:37 -0300

    Landing Page
    - Creación de la landing page.
    - Actualización del Redirect para que no quede en bucle cuando un usuario no logueado se encuentra en la landing page.
    - Actualización de App.tsx acorde a los cambios hechos.

---
+++ src/FRONT/views/components/heroSection.tsx
@@ -0,0 +1,32 @@
+
+import styles from "./heroSection.module.css";
+import Logo from "../../../assets/mechaLogo.png";
+import Interior from "../../../assets/interiorBarberia.avif";
+
+const HeroSection = () => {
+    return (
+        <section className={styles.section}>
+            <div className={styles.content}>
+                <img src={Logo} className={styles.logo} alt="Mecha's Barbershop Logo" /> 
+                <p className={styles.description}>
+                    En Mecha's Barbershop, nos dedicamos a ofrecer servicios de barbería de alta calidad en un ambiente acogedor y profesional. Nuestro equipo de barberos expertos está comprometido a brindarte el mejor corte de cabello y afeitado, adaptándose a tus necesidades y estilo personal.
+                </p>
+                <p className={styles.description}>
+                ¡Registrate para agendar tu primer turno!
+                </p>
+                <a href="/signUp" className={styles.primaryButton}>Crear Cuenta</a>
+                <p className={styles.description}>
+                ¡Inicia sesión si ya tienes una cuenta!
+                </p>
+                <a href="/login" className={styles.primaryButton}>Iniciar Sesión</a>
+
+            </div>
+            <div className={styles.mediaWrap}>
+                <img src={Interior} alt="Interior Barbershop" className={styles.image} />
+                <div className={styles.imageOverlay} />
+            </div>
+        </section>
+    );
+};
+
+export default HeroSection;
