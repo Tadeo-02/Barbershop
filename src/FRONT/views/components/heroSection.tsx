edf320de69e76536608d89418f538249bf9d8d87
dolodoming <doloodominguez@gmail.com>
2025-11-05 21:17:54 -0300

    Update heroSection.tsx

--- src/FRONT/views/components/heroSection.tsx
+++ src/FRONT/views/components/heroSection.tsx
@@ -1,37 +1,37 @@
-
 import styles from "./heroSection.module.css";
 import Interior from "../../../assets/interiorBarberia.avif";
+import { Link } from "react-router-dom";
 
 const HeroSection = () => {
     return (
-      <section className={styles.section}>
-        <div className={styles.content}>
-          <p className={styles.intro}>
-            Ofrecemos servicios de barbería de alta calidad, con barberos
-            expertos que se adaptan a tu estilo y necesidades.
-          </p>
-          <p className={styles.description}>
-            ¡Registrate para agendar tu primer turno!
-          </p>
-          <a href="/signUp" className={styles.primaryButton}>
-            Crear Cuenta
-          </a>
-          <p className={styles.description}>
-            ¡Inicia sesión si ya tienes una cuenta!
-          </p>
-          <a href="/login" className={styles.primaryButton}>
-            Iniciar Sesión
-          </a>
-        </div>
-        <div className={styles.mediaWrap}>
-          <img
-            src={Interior}
-            alt="Interior Barbershop"
-            className={styles.image}
-          />
-          <div className={styles.imageOverlay} />
-        </div>
-      </section>
+        <section className={styles.section}>
+            <div className={styles.content}>
+                <p className={styles.intro}>
+                    Ofrecemos servicios de barbería de alta calidad, con barberos expertos
+                    que se adaptan a tu estilo y necesidades.
+                </p>
+                <p className={styles.description}>
+                    ¡Registrate para agendar tu primer turno!
+                </p>
+                <Link to="/signUp" className={styles.primaryButton}>
+                    Crear Cuenta
+                </Link>
+                <p className={styles.description}>
+                    ¡Inicia sesión si ya tienes una cuenta!
+                </p>
+                <Link to="/login" className={styles.primaryButton}>
+                    Iniciar Sesión
+                </Link>
+            </div>
+            <div className={styles.mediaWrap}>
+                <img
+                    src={Interior}
+                    alt="Interior Barbershop"
+                    className={styles.image}
+                />
+                <div className={styles.imageOverlay} />
+            </div>
+        </section>
     );
 };
 
